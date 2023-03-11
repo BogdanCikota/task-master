8b2dd9212d30f07186a8bfb563f1755ae986bcb9
Bogdan Cikota <bcikota@live.com>
2023-03-11 20:02:29 +0100

    Add statistics logic on homePage

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase/firebaseConfig";
 
 function HomePage() {
+  const [employees, setEmployees] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const collectionEmployees = collection(db, "employees");
+    getDocs(collectionEmployees).then((snapshot) => {
+      if (snapshot.docs.length > 0) {
+        let employeesArr = [];
+
+        snapshot.docs.forEach((doc) => {
+          employeesArr.push({ ...doc.data(), id: doc.id });
+        });
+
+        setEmployees(employeesArr);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const collectionTasks = collection(db, "tasks");
+    getDocs(collectionTasks).then((snapshot) => {
+      if (snapshot.docs.length > 0) {
+        let tasksArr = [];
+
+        snapshot.docs.forEach((doc) => {
+          tasksArr.push({ ...doc.data(), id: doc.id });
+        });
+
+        setTasks(tasksArr);
+      }
+    });
+  }, []);
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1>HomePage</h1>
+      <div className="statistics">
+        <div>Total tasks: {tasks.length}</div>
+        <div>Total employees: {employees.length}</div>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
